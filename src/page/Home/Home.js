fbbfe0880d7cb221dba054b35e185a63d6928f31
letuyetngoc <letuyetngocksh@gmail.com>
2022-08-14 15:32:47 +0700

    build homeBottomLast.js and homeTestimonial.js

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import HomeAgents from './HomeAgents'
+import HomeBottomLast from './HomeBottomLast'
 import HomeFeature from './HomeFeature'
 import HomeHeader from './HomeHeader'
 import HomeInfo from './HomeInfo'
 import HomeService from './HomeService'
+import HomeTestimonial from './HomeTestimonial'
 import HomeUtility from './HomeUtility'
 
 export default function Home() {
@@ -15,6 +17,8 @@ export default function Home() {
             <HomeService />
             <HomeAgents />
             <HomeFeature />
+            <HomeBottomLast />
+            <HomeTestimonial />
             <br />
             <br />
             <br />
